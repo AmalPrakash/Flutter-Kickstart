545610eb7649497e917b3c22a530706259afa5dd
AmalPrakash <pkamalprakash98@gmail.com>
2020-07-26 14:11:11 +0530

    FAQ.dart and main page as faq

--- kickstart/lib/pages/faqs.dart
+++ kickstart/lib/pages/faqs.dart
@@ -1,30 +1,5 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:kickstart/datasource.dart';
-
-void main() {
-  runApp(MaterialApp(
-    home: FAQPage(),
-    theme: ThemeData(
-      primarySwatch: Colors.red,
-    ),
-  ));
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Kickstart Flutter"),
-      ),
-      body: Container(
-        child: Text("grocery app-nu kashtappedunnu.. sahayikkanam"),
-      ),
-    );
-  }
-}
 
 class FAQPage extends StatelessWidget {
   @override
